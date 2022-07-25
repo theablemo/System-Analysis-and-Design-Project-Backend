22c99ebca59e02ce1bb826b5923627a5d59d4f3a
amirreza <amirreza2001@gmail.com>
2022-07-25 18:59:13 +0430

    changing username to email field

--- SAD_backend/account/serializers/serializer_register.py
+++ SAD_backend/account/serializers/serializer_register.py
@@ -7,13 +7,13 @@ from base.serializer.parse_validate import parse_validate_name
 
 
 class RegisterSerializer(ModelSerializer):
-    email = serializers.CharField(allow_blank=False)
 
     class Meta:
         model = Member
-        fields = ['username', 'password', 'first_name', 'last_name', 'gender', 'email']
+        fields = ['password', 'first_name', 'last_name', 'gender', 'email']
 
     def validate(self, attrs):
+        print(attrs)
         attrs['first_name'] = parse_validate_name(attrs.get('first_name'), kind='first')
         attrs['last_name'] = parse_validate_name(attrs.get('last_name'), kind='last')
         attrs['password'] = make_password(attrs.get('password'))
