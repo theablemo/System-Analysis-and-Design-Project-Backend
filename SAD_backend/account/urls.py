c52209c4d349376dcfdad3f244db2fbe3f11ad85
amirreza <amirreza2001@gmail.com>
2022-07-25 19:09:50 +0430

    add check_username api

--- SAD_backend/account/urls.py
+++ SAD_backend/account/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from account.views import RegisterView, LoginView, MyTokenRefreshView
+from account.views import RegisterView, LoginView, MyTokenRefreshView, CheckUsernameView
 
 urlpatterns = [
     path('register', RegisterView.as_view(), name="register"),
     path('login', LoginView.as_view(), name='login'),
     path('refresh-token', MyTokenRefreshView.as_view(), name='refresh-token'),
+    path('check-username', CheckUsernameView.as_view(), name='check-username'),
 ]
 
