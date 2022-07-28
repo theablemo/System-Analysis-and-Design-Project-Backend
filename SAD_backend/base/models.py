0b9e36c667ccdabeff87b262478fcaf4ce6bbbed
mtndaghyani <matin.daghyani@gmail.com>
2022-07-28 12:48:35 +0430

    Implement Content model

--- SAD_backend/base/models.py
+++ SAD_backend/base/models.py
@@ -2,17 +2,8 @@ from django.db import models
 
 
 # Create your models here.
-
-class Content(models.Model):
-    pass
-
-
-class Attachment(models.Model):
-    pass
-
-
-class Library(models.Model):
-    pass
+from account.models import Member
+from backend.settings import CONTENTS_DIR
 
 
 class ContentType(models.Model):
@@ -20,4 +11,24 @@ class ContentType(models.Model):
 
 
 class AttachmentType(models.Model):
+    pass
+
+
+class Library(models.Model):
+    pass
+
+
+class Content(models.Model):
+    name = models.CharField()
+    member = models.ForeignKey(Member, on_delete=models.CASCADE)
+    date_created = models.DateField(verbose_name='date_created')
+    type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    library = models.ForeignKey(Library, on_delete=models.CASCADE)
+
+    @property
+    def path(self):
+        return f'{CONTENTS_DIR}{self.member.id}/{self.name}'
+
+
+class Attachment(models.Model):
     pass
