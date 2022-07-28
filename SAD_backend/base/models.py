d63e25991a1365c208f0875f842046684a83d65e
mtndaghyani <matin.daghyani@gmail.com>
2022-07-28 12:25:07 +0430

    Add base models

--- SAD_backend/base/models.py
+++ SAD_backend/base/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Content(models.Model):
+    pass
+
+
+class Attachment(models.Model):
+    pass
+
+
+class Library(models.Model):
+    pass
+
+
+class ContentType(models.Model):
+    pass
+
+
+class AttachmentType(models.Model):
+    pass
