05076361e12c6b5738e40c10c49f13d0f420fcc6
mtndaghyani <matin.daghyani@gmail.com>
2022-07-28 13:44:39 +0430

    Implement base models

--- SAD_backend/base/models.py
+++ SAD_backend/base/models.py
@@ -1,34 +1,45 @@
 from django.db import models
 
-
 # Create your models here.
 from account.models import Member
-from backend.settings import CONTENTS_DIR
+from base.utils.file_utils import content_file_path, attachment_file_path
 
 
 class ContentType(models.Model):
-    pass
+    name = models.CharField()
 
 
 class AttachmentType(models.Model):
-    pass
+    name = models.CharField()
 
 
 class Library(models.Model):
-    pass
+    name = models.CharField()
+    type = models.ForeignKey(ContentType,
+                             on_delete=models.CASCADE)
+    date_created = models.DateTimeField(verbose_name='date_created')
 
 
 class Content(models.Model):
-    name = models.CharField()
+    filename = models.CharField()
     member = models.ForeignKey(Member, on_delete=models.CASCADE)
-    date_created = models.DateField(verbose_name='date_created')
+    date_created = models.DateTimeField(verbose_name='date_created')
     type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
     library = models.ForeignKey(Library, on_delete=models.CASCADE)
+    file = models.FileField(upload_to=content_file_path)
 
     @property
     def path(self):
-        return f'{CONTENTS_DIR}{self.member.id}/{self.name}'
+        return content_file_path(self, self.filename)
 
 
 class Attachment(models.Model):
-    pass
+    filename = models.CharField()
+    member = models.ForeignKey(Member, on_delete=models.CASCADE)
+    type = models.ForeignKey(AttachmentType, on_delete=models.CASCADE)
+    content = models.ForeignKey(Content, on_delete=models.CASCADE)
+    file = models.FileField(upload_to=attachment_file_path)
+
+    @property
+    def path(self):
+        return attachment_file_path(self, self.filename)
