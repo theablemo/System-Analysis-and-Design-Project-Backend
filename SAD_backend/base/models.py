f90768ecff50f91fad4156ead749a3ffac9a10bf
mtndaghyani <matin.daghyani@gmail.com>
2022-07-28 13:53:23 +0430

    Set default type as 'other'

--- SAD_backend/base/models.py
+++ SAD_backend/base/models.py
@@ -8,15 +8,26 @@ from base.utils.file_utils import content_file_path, attachment_file_path
 class ContentType(models.Model):
     name = models.CharField()
 
+    @classmethod
+    def get_default_type_pk(cls):
+        obj, _ = cls.objects.get_or_create(name='other')
+        return obj.pk
+
 
 class AttachmentType(models.Model):
     name = models.CharField()
+
+    @classmethod
+    def get_default_type_pk(cls):
+        obj, _ = cls.objects.get_or_create(name='other')
+        return obj.pk
 
 
 class Library(models.Model):
     name = models.CharField()
     type = models.ForeignKey(ContentType,
-                             on_delete=models.CASCADE)
+                             default=ContentType.get_default_type_pk,
+                             on_delete=models.SET_DEFAULT)
     date_created = models.DateTimeField(verbose_name='date_created')
 
 
@@ -24,7 +35,9 @@ class Content(models.Model):
     filename = models.CharField()
     member = models.ForeignKey(Member, on_delete=models.CASCADE)
     date_created = models.DateTimeField(verbose_name='date_created')
-    type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    type = models.ForeignKey(ContentType,
+                             default=ContentType.get_default_type_pk,
+                             on_delete=models.SET_DEFAULT)
     library = models.ForeignKey(Library, on_delete=models.CASCADE)
     file = models.FileField(upload_to=content_file_path)
 
@@ -36,7 +49,9 @@ class Content(models.Model):
 class Attachment(models.Model):
     filename = models.CharField()
     member = models.ForeignKey(Member, on_delete=models.CASCADE)
-    type = models.ForeignKey(AttachmentType, on_delete=models.CASCADE)
+    type = models.ForeignKey(AttachmentType,
+                             default=AttachmentType.get_default_type_pk,
+                             on_delete=models.SET_DEFAULT)
     content = models.ForeignKey(Content, on_delete=models.CASCADE)
     file = models.FileField(upload_to=attachment_file_path)
 
