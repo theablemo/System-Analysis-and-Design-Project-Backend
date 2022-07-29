c984e7edf4804b1f3cfed45eb584dee46bdaab15
amirreza <amirreza2001@gmail.com>
2022-07-29 15:42:44 +0430

    adding swagger for get_user_info api

--- SAD_backend/account/views/view_get_user_info.py
+++ SAD_backend/account/views/view_get_user_info.py
@@ -8,7 +8,7 @@ from account.serializers import MemberInfoSerializer
 class GetUserInfoView(APIView):
     permission_classes = [IsAuthenticated]
 
-    @swagger_auto_schema(request_body=MemberInfoSerializer, responses={200: {}})
+    @swagger_auto_schema(responses={'200':MemberInfoSerializer})
     def get(self, request, *args, **kwargs):
         member_info = MemberInfoSerializer(request.user, context={'request': request}).data
 
