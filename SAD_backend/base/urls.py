5d77ac345e412eb1e5413707ec868f103f37845f
mtndaghyani <matin.daghyani@gmail.com>
2022-08-06 12:33:06 +0430

    Implement file download view

--- SAD_backend/base/urls.py
+++ SAD_backend/base/urls.py
@@ -4,5 +4,6 @@ from base.views import ContentView
 
 urlpatterns = [
     path('content/new/', ContentView.as_view(), name="new_content"),
+    path('<str:library>/<str:filename>/', ContentView.as_view(), name='download_content')
 
 ]
--- SAD_backend/base/views.py
+++ SAD_backend/base/views.py
@@ -1,3 +1,6 @@
+import mimetypes
+
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
@@ -6,6 +9,7 @@ from rest_framework.parsers import MultiPartParser, FileUploadParser
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+from base.models import Content
 from base.serializer.serializer_content import ContentSerializer
 
 
@@ -23,3 +27,18 @@ class ContentView(APIView):
         serializer.save()
         return Response({'message': 'Content created successfully.'}, status=status.HTTP_200_OK)
 
+    def get(self, request, library, filename):
+        try:
+            content = Content.objects.get(
+                member__id=request.user.pk,
+                library__name=library,
+                filename=filename
+            )
+            filepath = content.path
+            mime_type, _ = mimetypes.guess_type(filepath)
+            with open(filepath, 'r') as file:
+                response = HttpResponse(file, status=status.HTTP_200_OK, content_type=mime_type)
+                response['Content-Disposition'] = f'attachment; filename="{file.name.split("_")[-1]}"'
+                return response
+        except:
+            return Response({'message': 'Content not found'}, status=status.HTTP_404_NOT_FOUND)
