fac2d81b9dfbaef9de443f76bc0237afb42d462a
Alireza Eiji <alirezaeiji151379@gmail.com>
2022-08-08 19:45:10 +0430

    merge compltede

--- SAD_backend/content/views/view_add_content.py
+++ SAD_backend/content/views/view_add_content.py
@@ -16,12 +16,7 @@ class ContentView(APIView):
     def post(self, request):
         serializer = ContentSerializer(data=request.data, context={'request': request})
         serializer.is_valid(raise_exception=True)
-<<<<<<< HEAD:SAD_backend/content/views.py
-        serializer.save()
-        return Response({'message': 'Content created successfully.'}, status=status.HTTP_200_OK)
-=======
         content = serializer.save()
         content_info = ContentInfoSerializer(content, context={'request': request}).data
 
         return Response({'info': content_info, 'code': 'OK'})
->>>>>>> master:SAD_backend/content/views/view_add_content.py
