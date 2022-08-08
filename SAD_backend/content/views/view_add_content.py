f7f0c61438b63589b23a860d6038ccb5ef674f78
Alireza Eiji <alirezaeiji151379@gmail.com>
2022-08-08 19:45:44 +0430

    merge conflict resolved

--- SAD_backend/content/views/view_add_content.py
+++ SAD_backend/content/views/view_add_content.py
@@ -3,7 +3,6 @@ from rest_framework.parsers import MultiPartParser, FileUploadParser
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from rest_framework.permissions import AllowAny
 
 from content.serializer import ContentInfoSerializer
 from content.serializer.serializer_content import ContentSerializer
