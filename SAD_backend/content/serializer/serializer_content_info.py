0e0d09b67184ff94c5f75eba77378124b4172bee
amirreza <amirreza2001@gmail.com>
2022-08-11 19:28:49 +0430

    add category type to content serializer

--- SAD_backend/content/serializer/serializer_content_info.py
+++ SAD_backend/content/serializer/serializer_content_info.py
@@ -1,5 +1,6 @@
 from rest_framework.serializers import ModelSerializer
 from content.models import Content
+from content.models.content import ContentType
 
 
 class ContentInfoSerializer(ModelSerializer):
@@ -10,4 +11,5 @@ class ContentInfoSerializer(ModelSerializer):
     def to_representation(self, instance):
         data = super(ContentInfoSerializer, self).to_representation(instance)
         data['file_download_name'] = data['file'].split('/')[-1]
+        data['type_category'] = ContentType.objects.get(id=data['type']).type
         return data
