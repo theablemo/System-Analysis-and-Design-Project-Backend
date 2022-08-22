724108e85bacec9efcd9e17d86f69771cd6d6261
Alireza Eiji <AlirezaEiji151379@gmail.com>
2022-08-22 23:01:50 +0430

    git ignore changed added some batch files .yml file

---
+++ pipeline/pull_then_start.bat
@@ -0,0 +1,3 @@
+cd ../SAD_backend
+git pull origin master
+python manage.py runserver 0.0.0.0:8000
---
+++ pipeline/server_off.bat
@@ -0,0 +1,6 @@
+@ECHO OFF
+SET /A port=8000
+FOR /F "tokens=5" %%T IN ('netstat -ano ^| findstr :%port%') DO (
+    SET /A processid=%%T
+    TASKKILL /PID %%T /F
+)
