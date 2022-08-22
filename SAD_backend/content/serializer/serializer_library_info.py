a863723096f70bb5afa92751c34265abb426ccee
amirreza <amirreza2001@gmail.com>
2022-08-23 01:39:49 +0430

    adding file count to library info serializer

--- SAD_backend/content/serializer/serializer_library_info.py
+++ SAD_backend/content/serializer/serializer_library_info.py
@@ -1,8 +1,13 @@
 from rest_framework import serializers
-from content.models.content import Library
+from content.models.content import Library, Content
 
 
 class LibraryInfoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Library
         fields = '__all__'
+
+    def to_representation(self, instance):
+        data = super(LibraryInfoSerializer, self).to_representation(instance)
+        data['content_count'] = Content.objects.filter(library=instance).count()
+        return data
